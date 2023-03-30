838dcd5ec044622d1b8f2535cd1766d82b2e0d8d
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-03-30 17:30:06 +0200

    update_task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
 // Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const markup = galleryItems.map(item => {
+    return `<li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+   </a>
+</li>`
+});
+
+gallery.innerHTML = markup.join('');
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionsDelay: 250
+});
+gallery.addEventListener('click', (event) => {
+    event.preventDefault();
+});
