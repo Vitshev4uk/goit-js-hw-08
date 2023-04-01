705adf408040f68b5081b4e565d6d792fc456507
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-01 21:37:16 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+
+const iframe = document.getElementById('vimeo-player');
+console.log(iframe);
+
+const player = new Player(iframe);
+
+player.on('timeupdate', (data) => {
+    localStorage.setItem('videoplayer-current-time', 'data.seconds')
+});
+
+
+
+
+
 
