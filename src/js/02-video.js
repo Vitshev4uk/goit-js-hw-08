5346d50091d24d31ba6745ece65af9d1cefce34c
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-02 23:28:40 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -6,7 +6,29 @@ console.log(iframe);
 const player = new Player(iframe);
 
 player.on('timeupdate', (data) => {
-    localStorage.setItem('videoplayer-current-time', 'data.seconds')
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+    return;
+});
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+const savedTimeNum = Number.savedTime;
+console.log(savedTimeNum)
+
+console.log(savedTime)
+
+player.setCurrentTime(savedTime).then(function(seconds) {
+    // seconds = the actual time that the player seeked to
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            // the time was less than 0 or greater than the video’s duration
+            break;
+
+        default:
+            // some other error occurred
+            break;
+    }
 });
 
 
