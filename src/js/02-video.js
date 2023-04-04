cd7b5287597064ecb7ad2d153e40bc8f991469b9
Vitshev4uk <116673473+Vitshev4uk@users.noreply.github.com>
2023-04-04 13:55:48 +0200

    upd

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,5 +1,5 @@
 import Player from '@vimeo/player';
-var throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.getElementById('vimeo-player');
 console.log(iframe);
@@ -9,28 +9,14 @@ const player = new Player(iframe);
 const savedCurrentTime = (event) => {
     localStorage.setItem('videoplayer-current-time', event.seconds)
 };
-
 player.on('timeupdate', throttle(savedCurrentTime, 1000));
 
 const savedTime = localStorage.getItem('videoplayer-current-time');
+// const savedTimeNum = Number.savedTime;
+// console.log(savedTimeNum)
 
+player.setCurrentTime(savedTime);
 
-const savedTimeNum = Number.savedTime;
-console.log(savedTimeNum)
-
-player.setCurrentTime(savedTime).then(function(seconds) {
-    // seconds = the actual time that the player seeked to
-}).catch(function(error) {
-    switch (error.name) {
-        case 'RangeError':
-            // the time was less than 0 or greater than the video’s duration
-            break;
-
-        default:
-            // some other error occurred
-            break;
-    }
-});
 
 
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const messageInput = document.querySelector('textarea');
+
+form.addEventListener('input', throttle((event) => {
+    event.preventDefault();
+    const formFeedback = { email: input.value, message: messageInput.value }
+    localStorage.setItem('feedback-form-state', JSON.stringify(formFeedback));
+}, 500));
+const savedFeedback = localStorage.getItem('feedback-form-state');
+
+if (savedFeedback !== null) {
+    const parcedFeedback = JSON.parse(savedFeedback);
+    input.value = parcedFeedback.email;
+    messageInput.value = parcedFeedback.message;
+};
+
+form.addEventListener('submit', (event) => {
+    console.log(localStorage.getItem('feedback-form-state'));
+    event.preventDefault();
+    localStorage.setItem('feedback-form-state', '');
+    input.value = '';
+    messageInput.value = '';
+})
